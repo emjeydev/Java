5b0b230141f99fe4a20330d2b74acec154fbcdce
emjeydev <emjeydev@gmail.com>
2024-07-01 06:36:26 +0330

    pausing a thread simulation

--- 005 - Java Practice 02/JavaPractice/src/concurrency/DownloadFileTask.java
+++ 005 - Java Practice 02/JavaPractice/src/concurrency/DownloadFileTask.java
@@ -8,9 +8,17 @@ package concurrency;
 // Email: emjeydev@gmail.com
 // Github: emjeydev
 
-public class DownloadFileTask implements Runnable{
+public class DownloadFileTask implements Runnable {
     @Override
     public void run() {
         System.out.println("Downloading file... :" + Thread.currentThread().getName());
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Download complete: " + Thread.currentThread().getName());
     }
 }
